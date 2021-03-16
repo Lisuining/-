99998e29e1d90ba465af98b86f915810db022e4e
Lisuining <75533062+Lisuining@users.noreply.github.com>
2021-03-16 17:50:16 +0800

    Create mine.js

---
+++ mine.js
@@ -0,0 +1,100 @@
+// pages/mine/mine.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  new:function(e){
+    wx.redirectTo({
+      url: '/pages/ziliao/ziliao'
+    })
+  },
+
+  opinion:function(e){
+    wx.redirectTo({
+      url: '/pages/opinion/opinion'
+    })
+  },
+  gohome:function(e){
+    wx.redirectTo({
+      url: '/pages/home/home'
+    })
+  },
+
+  goorder:function(e){
+    wx.redirectTo({
+      url: '/pages/order/order'
+    })
+  },
+
+  gonews:function(e){
+    wx.redirectTo({
+      url: '/pages/news/news'
+    })
+  },
+
+  gomine:function(e){
+    wx.redirectTo({
+      url: '/pages/mine/mine'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
